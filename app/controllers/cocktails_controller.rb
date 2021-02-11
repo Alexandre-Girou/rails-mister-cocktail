84ec7d96d613f9678c74db1de4878f7f62913432
Alexandre Girou <agirou5@gmail.com>
2021-02-11 16:59:31 +0100

    Day 1 push

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,27 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+  def new
+    @cocktail = Cocktail.new
+  end
+  def create
+    @cocktail = Cocktail.new(strong_params)
+    if @cocktail.save
+      flash[:success] = "cocktail successfully created"
+      redirect_to cocktails_path
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end 
+
+  private
+
+  def strong_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create(name: "ice")
+mint = Ingredient.create(name: "mint leaves")
+
+mojito = Cocktail.create(name: "Mojito")
+cuba = Cocktail.create(name: "Cuba Libre")
+sex = Cocktail.create(name: "Sex on the Beach")
+
+Dose.create(description: "1oz", ingredient_id: lemon.id, cocktail_id: mojito.id)
+Dose.create(description: "3cl", ingredient_id: ice.id, cocktail_id: cuba.id)
+Dose.create(description: "2", ingredient_id: mint.id, cocktail_id: sex.id)
